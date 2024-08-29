9e15adf6713b49bfc181adca8726a09f527a53d2
shubeeth001 <shubeeth001@warren.k12.in.us>
2024-08-29 13:39:03 -0400

    summercat

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+    }
+
+    private static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            if (temperature >= 25 && temperature <= 45) {
+                return true;
+            }
+            return false;
+        } else {
+            if (temperature >= 25 && temperature <= 45) {
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+
+}
